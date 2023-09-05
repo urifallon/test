05dbf6aa0bae6ee01dbe870bb6fdabd42f2fc3b9
Uri <urifallon.sc.9125268@gmail.com>
2023-09-05 17:09:45 +0700

    Create base code
    Create base code

---
+++ math/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int select;
+            do
+            {
+                Console.WriteLine("Select: ");
+                Console.WriteLine("1. hinh chu nhat");
+                Console.WriteLine("2. hinh tron");
+                select = Convert.ToInt32(Console.ReadLine());
+            } while (select != 0);
+        }
+    }
+}
